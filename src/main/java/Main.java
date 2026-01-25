fc1f233f8154e826688de2c44feae2463d1e9a47
Max Tarkhanov <mtarkhanov666@gmail.com>
2026-01-25 19:15:09 +0300

    Calculator added

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,8 +2,15 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Введите текст и нажмите <Enter>: ");
-        String text = new Scanner(System.in) .nextLine();
-        System.out.println("Длина текста: " + text.length());
+        System.out.println("Введите первое число: ");
+        int number1 = new Scanner(System.in) .nextInt();
+        System.out.println("Введите второе число: ");
+        int number2 = new Scanner(System.in) .nextInt();
+        double quotient = (double) number1 / number2;
+        System.out.println("Результаты: ");
+        System.out.println(("Сложение : ")+(number1+number2));
+        System.out.println(("Вычитание : ")+(number1-number2));
+        System.out.println(("Умножение : ")+(number1*number2));
+        System.out.println(("Деление : ")+quotient);
     }
 }
